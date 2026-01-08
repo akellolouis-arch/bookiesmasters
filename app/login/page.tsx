fd77c3b5b820cb8940c4cb8680d8ef471d3cfc7e
emoit akelo <emoitakelo@gmail.com>
2026-01-08 08:18:09 +0300

    payment

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -18,7 +18,7 @@ export default function LoginPage() {
                 <form
                     action={async () => {
                         "use server"
-                        await signIn("google", { redirectTo: "/vip" })
+                        await signIn("google", { redirectTo: "/vip-tips" })
                     }}
                 >
                     <button
