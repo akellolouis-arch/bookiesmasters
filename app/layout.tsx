68018e2d0d1b09227a45ab8196f6b193b7a151fe
emoit akelo <emoitakelo@gmail.com>
2026-01-13 12:16:02 +0300

    payment

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import Script from "next/script";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
@@ -47,6 +48,28 @@ export default function RootLayout({
             gtag('config', 'G-N2MSB811W7');
           `}
         </Script>
+
+        {/* Meta Pixel */}
+        <Script id="meta-pixel" strategy="afterInteractive">
+          {`
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '690641200151405');
+            fbq('track', 'PageView');
+          `}
+        </Script>
+        <noscript>
+          <img height="1" width="1" style={{ display: 'none' }}
+            src="https://www.facebook.com/tr?id=690641200151405&ev=PageView&noscript=1"
+          />
+        </noscript>
+
         <AuthProvider>
           <Navbar />
           <main className="min-h-screen">{children}</main>
@@ -57,4 +80,4 @@ export default function RootLayout({
   );
 }
 
-import Script from "next/script";
+
