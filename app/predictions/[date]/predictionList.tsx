6fece10170aa9b8b17de5861a0df0ce835c7250f
emoit akelo <emoitakelo@gmail.com>
2026-06-12 12:52:05 +0300

    predictions

--- app/predictions/[date]/predictionList.tsx
+++ app/predictions/[date]/predictionList.tsx
@@ -199,10 +199,10 @@ export default function PredictionsList({
                   />
                 )}
                 <div className="flex flex-col truncate w-full leading-tight">
-                  <span className="font-medium text-xs text-amber-100 tracking-wide truncate drop-shadow-sm">
+                  <span className="font-normal text-xs text-amber-100 tracking-wide truncate drop-shadow-sm">
                     {league.name}
                   </span>
-                  <span className="text-[8px] text-emerald-200/70 font-semibold capitalize tracking-wider truncate">{league.country.toLowerCase()}</span>
+                  <span className="text-[8px] text-emerald-200/70 font-normal capitalize tracking-wider truncate">{league.country.toLowerCase()}</span>
                 </div>
               </div>
             </div>
