2a6651cc5c147fac672161799e5883651e660873
emoit akelo <emoitakelo@gmail.com>
2026-06-12 11:52:53 +0300

    predictions

--- app/predictions/[date]/predictionList.tsx
+++ app/predictions/[date]/predictionList.tsx
@@ -186,8 +186,8 @@ export default function PredictionsList({
         let globalIdx = 0;
         return safeData.map((league, idx) => (
           <div key={league.id || idx}>
-            <div className="flex items-center justify-center bg-gradient-to-r from-emerald-900/40 to-[#121212] py-2 px-3 shadow-md border border-white/5 border-b-0">
-              <div className="flex items-center justify-center gap-2">
+            <div className="flex items-center gap-1 bg-gradient-to-r from-emerald-900/40 to-[#121212] py-2 px-3 shadow-md border border-white/5 border-b-0">
+              <div className="flex items-center gap-2 w-full">
                 {league.logo && (
                   <Image
                     src={league.logo}
@@ -198,11 +198,11 @@ export default function PredictionsList({
                     unoptimized
                   />
                 )}
-                <div className="flex flex-col items-center justify-center truncate">
-                  <span className="font-medium text-xs text-amber-100 tracking-wide truncate drop-shadow-sm text-center">
+                <div className="flex flex-col truncate w-full">
+                  <span className="font-medium text-xs text-amber-100 tracking-wide truncate drop-shadow-sm">
                     {league.name}
                   </span>
-                  <span className="text-[8px] text-emerald-200/70 font-semibold capitalize tracking-wider truncate text-center">{league.country.toLowerCase()}</span>
+                  <span className="text-[8px] text-emerald-200/70 font-semibold capitalize tracking-wider truncate">{league.country.toLowerCase()}</span>
                 </div>
               </div>
             </div>
